92f31011ed4e47cdfea6de16e3030e0db2f5271c
Clement Chetaille <clement.chetaille@gmail.com>
2025-10-09 03:33:59 +0200

    SonoLight Rework Produits

---
+++ products-banner.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("products-banner").innerHTML = `
+<!-- ===== PRODUCTS BANNER ===== -->
+      <section class="products-banner">
+        <a href="casques.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/headphone.png" class="logo-products" />
+            <h3 id="text-icon">Casques</h3>
+          </div>
+        </a>
+        <a href="enceintes.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/speaker.png" class="logo-products" />
+            <h3 id="text-icon">Enceintes</h3>
+          </div>
+        </a>
+        <a href="microphones.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/microphone.png" class="logo-products" />
+            <h3 id="text-icon">Microphones</h3>
+          </div>
+        </a>
+        <a href="tabledemix.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/mixing-table.png" class="logo-products" />
+            <h3 id="text-icon">Tables de mixage</h3>
+          </div>
+        </a>
+        <a href="interfaceaudio.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/audio-interface.png" class="logo-products" />
+            <h3 id="text-icon">Interface Audio</h3>
+          </div>
+        </a>
+        <a href="materieldj.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/tripod.png" class="logo-products" />
+            <h3 id="text-icon">Pied & Support</h3>
+          </div>
+        </a>
+        <a href="materieldj.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/dj-booth.png" class="logo-products" />
+            <h3 id="text-icon">DJ</h3>
+          </div>
+        </a>
+        <a href="lumieres.html" class="icon-products-link">
+          <div class="icon-products">
+            <img src="images/spotlight.png" class="logo-products" />
+            <h3 id="text-icon">Lumières</h3>
+          </div>
+        </a>
+      </section>
+    </section>
+  `;
+});
